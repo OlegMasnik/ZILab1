6a9a001ecd6c2c96f3686526f78497f78853855c
OlegMasnik <olegmasnik@gmail.com>
2016-03-16 20:29:05 +0200

    Update MainActivity.java
    remove space

--- app/src/main/java/ua/home/kubic/zilab1/MainActivity.java
+++ app/src/main/java/ua/home/kubic/zilab1/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     EditText result;
     Button encrypt;
 
-    int n = 33;
-    private char [] upper = {' ', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К',
+    int n = 32;
+    private char [] upper = {'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К',
             'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я'};
     private char [] lower = {'а', 'б', 'в', 'г', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к',
             'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я'};
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                                 res[i] = upper[(j + k) % n];
                                 break;
                             }else if (c == lower[j]){
-                                res[i] = lower[(j + k) % (n - 1)];
+                                res[i] = lower[(j + k) % (n)];
                                 break;
                             }else {
                                 res[i] = c;
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                                 res[i] = upper[((j - k + n) % (n))];
                                 break;
                             }else if (c == lower[j]){
-                                res[i] = lower[((j - k + n - 1) % (n - 1))];
+                                res[i] = lower[((j - k + n) % (n))];
                                 break;
                             }else {
                                 res[i] = c;
